fbf55712b221d13c2eadc42058a57b068349b43e
GHANIE\abdal <abdallahabdulghany@gmail.com>
2024-06-30 15:46:11 +0300

    Update .gitignore & Edit loss functions and main

--- src/utils/DiceLoss.py
+++ src/utils/DiceLoss.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn as nn
 
 class DiceLoss(nn.Module):
-    def __init__(self, smooth=1e-5):
+    def __init__(self, smooth=1.0):
         super(DiceLoss, self).__init__()
         self.smooth = smooth
 
     def forward(self, outputs, targets):
-        outputs = torch.sigmoid(outputs)
-        intersection = (outputs * targets).sum(dim=(2, 3, 4))
-        dice_coef = (2. * intersection + self.smooth) / (outputs.sum(dim=(2, 3, 4)) + targets.sum(dim=(2, 3, 4)) + self.smooth)
-        dice_loss = 1 - dice_coef
-        return dice_loss.mean()
+        outputs = torch.sigmoid(outputs)  # Apply sigmoid to get probabilities in range [0, 1]
+        outputs = outputs.contiguous().view(-1)  # Flatten the tensor
+        targets = targets.contiguous().view(-1)  # Flatten the tensor
+        
+        intersection = (outputs * targets).sum()  # Compute intersection
+        dice = (2. * intersection + self.smooth) / (outputs.sum() + targets.sum() + self.smooth)  # Compute Dice coefficient
+        return 1 - dice  # Return Dice loss
