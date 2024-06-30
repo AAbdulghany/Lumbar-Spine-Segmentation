44fd24136f32babd9de83874bbca1efc12d37853
GHANIE\abdal <abdallahabdulghany@gmail.com>
2024-06-30 15:03:41 +0300

    create dataset for nnunet

--- src/nnunet/create_dataset_json.py
+++ src/nnunet/create_dataset_json.py
@@ -6,29 +6,35 @@ base_dir = '/Users/saiffouda/WorkingSpace_code/DL/Project/Structure/nnUNet_raw/D
 
 # Define the dataset information
 dataset = {
-    "name": "Lumbar Spine Segmentation",
-    "description": "Segmentation of vertebrae, intervertebral discs, and spinal canal in lumbar spine MRI",
-    "reference": "Lumbar SPIDER Challenge",
-    "licence": "CC-BY-SA 4.0",
-    "release": "0.0",
-    "tensorImageSize": "4D",
-    "modality": {
-        "0": "MRI_T1",
-        "1": "MRI_T2"
+    "channel_names": {
+        "0": "MRI"
     },
     "labels": {
-        "0": "background",
-        "1": "vertebrae",
-        "2": "intervertebral discs",
-        "3": "spinal canal"
+        "background": 0,
+        "C1": 1,
+        "C2": 2,
+        "C3": 3,
+        "C4": 4,
+        "C5": 5,
+        "C6": 6,
+        "C7": 7,
+        "C8": 8,
+        "C9": 9,
+        "C10": 10,
+        "C11": 11,
+        "C12": 12,
+        "C13": 13,
+        "C14": 14,
+        "C15": 15,
+        "C16": 16,
+        "C17": 17,
+        "C18": 18,
+        "C19": 19,
+        "C20": 20
     },
-    "channel_names": {
-        "0": "T1",
-        "1": "T2"
-    },
-    "numTraining": 10,
-    "numTest": 3,
+    "numTraining": 9,
     "file_ending": ".nii.gz",
+    "overwrite_image_reader_writer": "SimpleITKIO",
     "training": [],
     "test": []
 }
@@ -40,27 +46,25 @@ imagesTs_path = os.path.join(base_dir, 'imagesTs')
 labelsTs_path = os.path.join(base_dir, 'labelsTs')
 
 # Create training entries
-for i in range(1, 11):
-    case = f"{i:03}"
+training_cases = [
+    "BRATS_001", "BRATS_002", "BRATS_003", "BRATS_004", "BRATS_005",
+    "BRATS_007", "BRATS_008", "BRATS_009", "BRATS_010"
+]
+
+for case in training_cases:
     training_entry = {
-        "image": [
-            os.path.join(imagesTr_path, f"BRATS_{case}_0000.nii.gz"),
-            os.path.join(imagesTr_path, f"BRATS_{case}_0001.nii.gz")
-        ],
-        "label": os.path.join(labelsTr_path, f"BRATS_{case}.nii.gz")
+        "image": [os.path.join(imagesTr_path, f"{case}_0000.nii.gz")],
+        "label": os.path.join(labelsTr_path, f"{case}.nii.gz")
     }
     dataset["training"].append(training_entry)
 
 # Create test entries
-for i in range(11, 14):
-    case = f"{i:03}"
+test_cases = ["BRATS_011", "BRATS_012", "BRATS_013"]
+
+for case in test_cases:
     test_entry = {
-        "image": [
-            os.path.join(imagesTs_path, f"BRATS_{case}_0000.nii.gz"),
-            os.path.join(imagesTs_path, f"BRATS_{case}_0001.nii.gz"),
-            os.path.join(imagesTs_path, f"BRATS_{case}_0002.nii.gz")
-        ],
-        "label": os.path.join(labelsTs_path, f"BRATS_{case}.nii.gz")
+        "image": [os.path.join(imagesTs_path, f"{case}_0000.nii.gz")],
+        "label": os.path.join(labelsTs_path, f"{case}.nii.gz")
     }
     dataset["test"].append(test_entry)
 
